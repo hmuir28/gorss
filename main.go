96a893f5d479886e424eddf39f4f614b9ecddb00
Harry Muir Camacho <hmuir@Harrys-MacBook-Pro.local>
2023-06-10 19:23:59 -0600

    Added server && health endpoint

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello World")
-
 	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
@@ -19,5 +19,32 @@ func main() {
 		log.Fatal("PORT was not found in the env vars")
 	}
 
-	fmt.Println("PORT: ", portString)
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/health", handleReadiness)
+
+	router.Mount("/v1", v1Router)
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
